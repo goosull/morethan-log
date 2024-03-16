e0e7e98dc72515cdf6e6b14800e121e954b1a462
goosull <134674369+goosull@users.noreply.github.com>
2024-03-16 20:52:54 +0900

    modified:   src/pages/_document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,9 +37,8 @@ class MyDocument extends Document {
             </>
           )}
           {/* google ad sense*/}
-          <meta name="google-adsense-account" content="ca-pub-1720871640755874"></meta>
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1720871640755874"
-     crossorigin="anonymous"></script>
+          <meta name="google-adsense-account" 
+                content="ca-pub-1720871640755874"></meta>
         </Head>
         <body>
           <Main />
