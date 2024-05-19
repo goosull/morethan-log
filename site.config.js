8f87960c1941a5dd0fc315c9a6ca4a764ae09606
goosull <134674369+goosull@users.noreply.github.com>
2024-05-19 18:17:30 +0900

    modified:   site.config.js

--- site.config.js
+++ site.config.js
@@ -12,10 +12,10 @@ const CONFIG = {
     ootd: "https://www.musinsa.com/snap/profile/f14e78bef7d8028c6f0fe8cfee9920d5",
   },
   projects: [
-    {
-      name: `Software_Engineering_Itust2`,
-      href: "https://github.com/goosull/Software_Engineering_Itrust2",
-    },
+    // {
+    //   name: `Software_Engineering_Itust2`,
+    //   href: "https://github.com/goosull/Software_Engineering_Itrust2",
+    // },
   ],
 
   // blog setting (required)
